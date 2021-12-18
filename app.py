daa15bfce244e06a9fa9039a7d14705d3fa81635
solos-pro <sergey-solonichenko@rambler.ru>
2021-12-18 19:39:43 +0300

    tag function doesn't work.

--- app.py
+++ app.py
@@ -10,4 +10,17 @@ def page_index():
     # return 'server works'
     return render_template('index.html', posts=posts)
 
+@app.route("/search/")
+def search_page():
+    name = request.args.get("s")
+    with open("candidates.json") as f:
+        candidates = json.load(f)
+    users = []
+    if name:
+        for candidate in candidates:
+            if name in candidate["name"]:
+                users.append(candidate["name"])
+
+    return render_template("search.html", users=users, cnt=len(users))
+
 app.run(debug=True)
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def load_data():
     with open('data/comments.json') as fp:
         comments = json.load(fp)
 
-    posts = attach_comments_to_posts(posts, comments)
+    posts = prepare_posts(posts, comments)
 
     with open('data/bookmarks.json') as fp:
         bookmark = json.load(fp)
@@ -18,7 +18,7 @@ def load_data():
 
 
 
-def attach_comments_to_posts(posts, comments):
+def prepare_posts(posts, comments):
     for i, post in enumerate(posts):
         pk = post.get('pk')
         post_comments = []
@@ -26,6 +26,22 @@ def attach_comments_to_posts(posts, comments):
             if comment.get('post_id') == pk:
                 post_comments.append(comment)
             posts[i]['comment_count'] = len(post_comments)
+
+        posts[i]['content'] = tegify_content(posts[i]['content'])
+        pprint(posts)
     return posts
 
-pprint(load_data())
+# pprint(load_data())
+
+def tegify_content(content):
+    words = content.split(" ")
+    for i, word in enumerate(words):
+        if word.startswith("#"):
+            tag = word.replace("#", "")
+            link = f"<a href='/tag/{tag}>'{word}</a>"
+            words[i] = link
+    print(words)
+    print(" ".join(words))
+    return " ".join(words)
+
+tegify_content("#egg #peece world #travel")
